c051f87a465b71a9a5bbf6bcba58ecdb33876b41
kahouais <kahloriane@gmail.com>
2020-11-19 10:27:49 +0100

    header

--- header.php
+++ header.php
@@ -6,54 +6,42 @@
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/site.css"></style>
-        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/header.css"></style>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/header.css"></style>
+
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
         <?php wp_head(); ?>
-
     </head>
-    <!--<body>
+
+    <body>
       <header>
-        <div class="concept concept-three">
-    <div class="word">
-      <div class="hover">
-
-  </div>
-        <fieldset>
-            <h1><?php //bloginfo( 'name' ); ?></h1>
-
-        <nav id="menuprincipal">
-          <div id="legendemenuprincipal"></div>
-          <?php //wp_page_menu('show_home=1'); ?>
-        </nav>
-      </fieldset>
-    </header>-->
-<body>
-<header>
-  <section>
-
-  <div class="headercontainer">
-    <div class="logo">
-      <img src="<?php echo get_template_directory_uri();?>/images/test.png" alt="logo m2a maçonnerie d'antan et d'aujoud'hui">
-    </div>
-    <section class="contact">
-      <div class="tel">
-			        <img src="<?php echo get_template_directory_uri();?>/images/tel.png">
-			        <p class="texteicone">06 74 11 63 45</p>
-			      </div>
-            <div class="mail">
-			        <img src="<?php echo get_template_directory_uri();?>/images/mail.png">
-			        <p class="texteicone">pa.flamens.@orange.fr</p>
-			    </div>
-          <div class="boutondevis">
-            <div class ="rectangle">
-              <p class="textebouton">Demande de devis</p>
-            </div>
+        <div class="wrap">
+          <div class="logo">
+          <img src="<?php echo get_template_directory_uri();?>/images/test.png" alt="logo m2a maçonnerie d'antan et d'aujoud'hui">
           </div>
+          <div class="navigation">
+          <div class="contact">
+            <div class="tel">
+			               <!--<img src="<?php echo get_template_directory_uri();?>/images/tel.png">-->
+			                <p>06 74 11 63 45</p>
+			        </div>
 
-          <nav id="menuprincipal">
-            <div id="legendemenuprincipal">
-              <div class="menu">
-                <ul>
+              <div class="mail">
+			             <!--<img src="<?php echo get_template_directory_uri();?>/images/mail.png">-->
+			                <p>pa.flamens.@orange.fr</p>
+			        </div>
+
+              <div class="boutondevis">
+                  <div class ="rectangle">
+                    <p>Demande de devis</p>
+                  </div>
+              </div>
+
+
+            </div>
+            <div class="menuprincipal">
+
+               <div class="menu">
+                 <ul>
                   <li class="current_page_item">
                     <a href="http://m4aderys.fr/">Accueil</a>
                   </li>
@@ -70,17 +58,10 @@
                     <a href="http://m4aderys.fr/contact/">Contact</a>
                   </li>
                 </ul>
+               </div>
             </div>
-
-
-          </nav>
-
-
-    </div>
-  </div>
-</section>
-
-
-
-</header>
-</body>
+          </div>
+        </div>
+      </header>
+    </body>
+  </html>
